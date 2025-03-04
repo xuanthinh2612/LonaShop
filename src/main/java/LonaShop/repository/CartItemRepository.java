65e3b3e737e514126d27e41e184fc3a5ebffe85e
thinh-dell-pc <xuanthinh2612@gmail.com>
2025-03-04 19:00:29 +0900

    add order item cart item

--- src/main/java/LonaShop/repository/CartItemRepository.java
+++ src/main/java/LonaShop/repository/CartItemRepository.java
@@ -1,19 +1,9 @@
 package LonaShop.repository;
 
-import LonaShop.model.Article;
+import LonaShop.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartItemRepository extends JpaRepository<Article, Long> {
-//
-//    // Status 1: public 0 : unPublic
-//    @Query(value = "select * from article where status = 1", nativeQuery = true)
-//    List<Article> findAvilableList();
-//
-//    @Query(value = "select count(*) from article where status = 1", nativeQuery = true)
-//    int countPublic();
-//
-//    @Query(value = "select count(*) from article where status = 0", nativeQuery = true)
-//    int countNonPublic();
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 }
